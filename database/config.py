bfdb3d001629b66d5ea25c92d10d2f390a625b1c
Vikki Kumar <vikkikumar1@gmail.com>
2026-05-28 16:26:56 +0530

    Fixed render enviornment variables

--- database/config.py
+++ database/config.py
@@ -1,9 +1,10 @@
-import streamlit as st
+import os
+from supabase import create_client
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-from supabase import create_client, Client
-
-supabase: Client = create_client(
-    st.secrets["SUPABASE_URL"],
-    st.secrets["SUPABASE_KEY"]
+supabase = create_client(
+    SUPABASE_URL,
+    SUPABASE_KEY
 )
